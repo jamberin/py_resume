35ae62760ae29aa9294f6c3ca8feff5fd02860c9
James Beringer <jamberin@gmail.com>
2021-01-18 23:08:11 -0500

    [CAPTCHA] Adding integration for captcha validations

--- application/application_router.py
+++ application/application_router.py
@@ -1,15 +1,46 @@
 """ Main router for the application """
+import requests
+
 from flask import Flask, render_template, request, flash
-from application.functions.contact_form import contact_form
 from utils_package.py_utils.logger import logger
 
+from application.functions.contact_form import contact_form
+
+from application.base_configurations import VARS
+
 app = Flask(__name__, template_folder='static')
-app.secret_key = 'this_secret_key'
+app.secret_key = VARS['application.keys.secret_key']
 
 # HTML Class Definitions
 set_current = 'current'
 set_error = 'display_error'
 set_success = 'display_success'
+
+# reCaptcha Variables
+site_key = VARS['captcha.keys.site_key']
+secret_key = VARS['captcha.keys.secret_key']
+
+## TEST PORTION
+# Test Page
+@app.route('/test')
+def test_page():
+    return render_template('pages/zz_sample_output.html', site_key=site_key)
+
+
+@app.route('/handle', methods=['POST'])
+def lists():
+    parameters = request.form
+
+    recaptcha_passed = False
+    recaptcha_response = parameters.get('g-recaptcha-response')
+    try:
+        # recaptcha_secret = os.environ.get('RECAPTCHA_SECRET')
+        response = requests.post(f'https://www.google.com/recaptcha/api/siteverify?secret='
+                                 f'{secret_key}&response={recaptcha_response}').json()
+        recaptcha_passed = response.get('success')
+        return render_template('pages/zz_test_data_output.html', result=response)
+    except Exception as e:
+        print(f"failed to get reCaptcha: {e}")
 
 
 # Home Page - Main Index
@@ -64,7 +95,7 @@ def tech_exp_page():
 # LINKS SUBPAGES
 @app.route('/links/contact')
 def contact_form_page():
-    return render_template('pages/contact_form.html', links_class=set_current)
+    return render_template('pages/contact_form.html', links_class=set_current, site_key=site_key, secret_key=secret_key)
 
 
 # APPLICATION INTEGRATION
@@ -72,6 +103,17 @@ def contact_form_page():
 def contact_form_input():
     if request.method == 'POST':
         result = request.form
+        captcha_response = result.get('g-recaptcha-response')
+        try:
+            response = requests.post(f'https://www.google.com/recaptcha/api/siteverify?secret='
+                                     f'{secret_key}&response={captcha_response}').json()
+            if response.get('success') is False:
+                logger.error('reCaptcha failed...')
+                flash('reCaptcha failed. If this is a mistake, contact me directly beringersolutions@gmail.com')
+                return render_template('pages/contact_form.html', links_class=set_current,
+                                       message_display_class=set_error)
+        except Exception as e:
+            logger.error(f'Failed to get reCaptcha: {e}')
         response = contact_form.contact_form_submission(result['name'], result['email'], result['message'])
         logger.info('Form submission status code: ' + str(response['status_code']))
         logger.info('Form submission display message: ' + response['display_message'])
